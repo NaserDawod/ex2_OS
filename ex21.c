95c82cfaec1ad8c8b21200087ae23406d9b5facf
NaserDawod <90802273+NaserDawod@users.noreply.github.com>
2022-05-14 15:57:38 +0300

    Add files via upload

--- ex21.c
+++ ex21.c
@@ -1,53 +1,78 @@
 #include <stdio.h>
-//#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-char read_char(int fd, int close_fd2) {
-    char c = ' ';
-    int check = 1;
-    while (c == '\n' || c == ' '){
-        check = read(fd, &c, 1);
-        if (check < 0) {
-            close(fd);
-            close(close_fd2);
-            exit(0);
-        } else if(check == 0){
-            return 0;
-        }
+int main(int argc, char** argv) {
+    if(argc != 3){
+        exit(-1);
     }
-    return c;
-}
-
-int main2(int argc, char** argv) {
-//    if(argc != 3){
-//        exit(2);
-//    }
     size_t check = 1, check2 = 1;
     int file1 = open(argv[1], 'r');
     if (file1 < 0){
-        exit(2);
+        exit(-1);
     }
     int file2 = open(argv[2], 'r');
     if (file2 < 0){
         close(file1);
-        exit(2);
+        exit(-1);
     }
     char c1, c2;
-    int similar;
-    do {
-        c1 = read_char(file1, file2);
-        c2 = read_char(file2, file1);
-        if((c1 == 0 && c2 != 0) || (c1 != 0 && c2 == 0) || (c1 != c2 && abs(c1 - c2) != 32)) {
+    int similar = 0;
+    while (1) {
+        check = read(file1, &c1, 1);
+        if (check < 0) {
+            close(file1);
+            close(file2);
+            exit(-1);
+        }
+        check2 = read(file2, &c2, 1);
+        if (check2 < 0) {
+            close(file1);
+            close(file2);
+            exit(-1);
+        }
+        if (check == 0 && check2 == 0){
+            break;
+        }
+        if ((check == 0 && c2 != ' ' && c2 != '\n' ) || (check2 == 0 && c1 != ' ' && c1 != '\n') || (c1 != c2 && abs(c1 - c2) != 32
+                                                                                                     && c1 != ' ' && c1 != '\n' && c2 != ' ' && c2 != '\n')) {
             close(file1);
             close(file2);
             return 2;
-        } else if (abs(c1 - c2) == 32) {
-            similar = 1;
         }
-    }while (c1 != 0 && c2 != 0);
+        if ((c1 == ' ' && c2 != ' ') || (c1 != ' ' && c2 == ' ') || (c1 == '\n' && c2 != '\n') || (c1 != '\n' && c2 == '\n')
+            || (abs(c1 - c2) == 32)) {
+            similar = 1;
+            if (c1 == '\n' || c1 == ' ') {
+                while (c1 == '\n' || c1 == ' '){
+                    check = read(file1, &c1, 1);
+                    if (check < 0) {
+                        close(file1);
+                        close(file2);
+                        exit(-1);
+                    } else if(check == 0) {
+                        break;
+                    }
+                }
+            }
+            if (c2 == '\n' || c2 == ' ') {
+                while (c2 == '\n' || c2 == ' '){
+                    check = read(file2, &c2, 1);
+                    if (check < 0) {
+                        close(file1);
+                        close(file2);
+                        exit(-1);
+                    } else if(check == 0) {
+                        break;
+                    }
+                }
+            }
+            if (check != 0 && check2 != 0 && c1 != c2 && abs(c1 - c2) != 32){
+                return 2;
+            }
+        }
+    }
     close(file1);
     close(file2);
     if(similar == 1){
